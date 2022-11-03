cec02e68d01b1150d82a73ba6bc0037cbd1d0187
Andrés Alfaro Fernández <andresalfe@gmail.com>
2022-11-03 07:07:13 +0100

    Initial Commit

---
+++ Practica4-parte1/Practica4_parte1App.swift
@@ -0,0 +1,17 @@
+//
+//  Practica4_parte1App.swift
+//  Practica4-parte1
+//
+//  Created by Andrés Alfaro Fernández on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Practica4_parte1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Practica4-parte1UITests/Practica4_parte1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Practica4_parte1UITestsLaunchTests.swift
+//  Practica4-parte1UITests
+//
+//  Created by Andrés Alfaro Fernández on 3/11/22.
+//
+
+import XCTest
+
+final class Practica4_parte1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
