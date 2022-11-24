9099911f967f00e97374e768ae4b1675089a6a89
Andrés Alfaro Fernández <andresalfe@gmail.com>
2022-11-24 19:25:44 +0100

    vertical and horizontal

--- Practica4-parte1/Views/AnswerView.swift
+++ Practica4-parte1/Views/AnswerView.swift
@@ -16,9 +16,19 @@ struct AnswerView: View {
     @State var answer: String = ""
     @State var showAlert = false
     
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
     var body: some View {
-        
-        VStack{
+        if verticalSizeClass == .compact{
+            horizontalView
+        }else{
+            verticalView
+        }
+    }
+    
+    private var verticalView: some View {
+        return VStack{
             HStack{
                 
                 //Pregunta
@@ -30,7 +40,6 @@ struct AnswerView: View {
                     .resizable()
                     .frame(width: 25, height: 25)
                     .scaledToFit()
-                
             }
             
             TextField("Respuesta",
@@ -38,7 +47,7 @@ struct AnswerView: View {
                       onCommit: {
                 showAlert = true
             }
-                
+                      
             )
             .padding(40)
             .overlay (RoundedRectangle (cornerRadius: 20).stroke(lineWidth: 1))
@@ -62,36 +71,8 @@ struct AnswerView: View {
                 Text("Comprobar")
             }
             
-        } // end HStack
-        //Imagen de la pregunta
-        AsyncImage(url: quizItem.attachment?.url){ phase in // 1
-            if let image = phase.image { // 2
-                // if the image is valid
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-            } else if phase.error != nil { // 3
-                // some kind of error appears
-                Text("No image available")
-            } else {
-                //appears as placeholder image
-                Image(systemName: "photo.circle.fill") // 4
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            }
-        }.frame(width: 360, height: 240, alignment: .center)
-            .scaledToFit()
-            .clipShape (RoundedRectangle (cornerRadius: 20))
-            .overlay (RoundedRectangle (cornerRadius: 20).stroke(lineWidth: 4))
-            .padding(30)
-        
-        
-        HStack{
-            //Score
-            Text("Score: \(scoresModel.acertadas.count)")
-            
-            //Imagen del autor
-            AsyncImage(url: quizItem.author?.photo?.url){ phase in // 1
+            //Imagen de la pregunta
+            AsyncImage(url: quizItem.attachment?.url){ phase in // 1
                 if let image = phase.image { // 2
                     // if the image is valid
                     image
@@ -106,45 +87,133 @@ struct AnswerView: View {
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                 }
-            }.frame(width: 50, height: 50)
-            .clipShape (Circle ())
-            
-        }
-        
-        
-    }
-}
-            
-            
-            
-            /*
-            
-            VStack{
-                    HStack{
-                        //Imagen de la pregunta
-                        
-                        VStack{
-                            HStack{
-                                //Boton de favoritos
-                                //Imagen del autor
-                                
-                                //Nombre del autor
-                                Text(quizItem.author?.username ?? "Someone")
-                                    .font(.system(size: 12, weight: .light))
-                            }
-                            
-                            HStack{
-                                //Pregunta
-                                
-                            }
-                        }
+            }.frame(width: 360, height: 240, alignment: .center)
+                .scaledToFit()
+                .clipShape (RoundedRectangle (cornerRadius: 20))
+                .overlay (RoundedRectangle (cornerRadius: 20).stroke(lineWidth: 4))
+                .padding(30)
+       
+            HStack{
+                //Score
+                Text("Score: \(scoresModel.acertadas.count)")
+                
+                //Imagen del autor
+                AsyncImage(url: quizItem.author?.photo?.url){ phase in // 1
+                    if let image = phase.image { // 2
+                        // if the image is valid
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    } else if phase.error != nil { // 3
+                        // some kind of error appears
+                        Text("No image available")
+                    } else {
+                        //appears as placeholder image
+                        Image(systemName: "photo.circle.fill") // 4
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
                     }
+                }.frame(width: 50, height: 50)
+                    .clipShape (Circle ())
+            }
+        }
+    }
+    
+    private var horizontalView: some View {
+        return HStack{
+            VStack{
+                //Imagen de la pregunta
+                AsyncImage(url: quizItem.attachment?.url){ phase in // 1
+                    if let image = phase.image { // 2
+                        // if the image is valid
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    } else if phase.error != nil { // 3
+                        // some kind of error appears
+                        Text("No image available")
+                    } else {
+                        //appears as placeholder image
+                        Image(systemName: "photo.circle.fill") // 4
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }.frame(width: 360, height: 240, alignment: .center)
+                    .scaledToFit()
+                    .clipShape (RoundedRectangle (cornerRadius: 20))
+                    .overlay (RoundedRectangle (cornerRadius: 20).stroke(lineWidth: 4))
+                    .padding(30)
+            }
+            VStack{
+                HStack{
+                    
+                    //Pregunta
+                    Text(quizItem.question)
+                        .font(.system(size: 20, weight: .bold))
+                    
+                    //Boton de favoritos
+                    Image(quizItem.favourite ? "star_yellow" : "star_grey")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .scaledToFit()
                 }
-            
-        
+                
+                TextField("Respuesta",
+                          text: $answer,
+                          onCommit: {
+                    showAlert = true
+                }
+                          
+                )
+                .padding(40)
+                .overlay (RoundedRectangle (cornerRadius: 20).stroke(lineWidth: 1))
+                .padding(30)
+                .alert(isPresented: $showAlert) {
+                    let string1 = quizItem.answer.lowercased().trimmingCharacters(in:
+                            .whitespacesAndNewlines)
+                    let string2 = answer.lowercased().trimmingCharacters(in:
+                            .whitespacesAndNewlines)
+                    return Alert(title: Text("Resultado"),
+                                 message: Text(string1 == string2 ? "Correcta" : "Incorrecta"),
+                                 dismissButton: .default(Text("Ok"))
+                    )
+                }
+                
+                Button(action: {
+                    showAlert = true
+                    scoresModel.check(respuesta: answer, quiz: quizItem)
+                    scoresModel.score()
+                }) {
+                    Text("Comprobar")
+                }
+                
+                HStack{
+                    //Score
+                    Text("Score: \(scoresModel.acertadas.count)")
+                    
+                    //Imagen del autor
+                    AsyncImage(url: quizItem.author?.photo?.url){ phase in // 1
+                        if let image = phase.image { // 2
+                            // if the image is valid
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                        } else if phase.error != nil { // 3
+                            // some kind of error appears
+                            Text("No image available")
+                        } else {
+                            //appears as placeholder image
+                            Image(systemName: "photo.circle.fill") // 4
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                    }.frame(width: 50, height: 50)
+                        .clipShape (Circle ())
+                }
+            }
+        }
     }
 }
-*/
 /*
 struct QuizPlayView_Previews: PreviewProvider {
     static var previews: some View {
